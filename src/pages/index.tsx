e650dc3b2d3cb6394658c14f552bd13c6683544a
Dmitry Lyapin <lyapindm@yandex.ru>
2025-09-08 12:53:59 +0300

    Add client first page document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,8 +20,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Перейти в документацию
+            to="/intro">
+            Руководство пользователей
           </Link>
         </div>
       </div>
