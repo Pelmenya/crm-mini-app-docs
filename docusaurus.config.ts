c2b5e302ed8809f07346daff8bf8db7e0ee669fd
Dmitry Lyapin <lyapindm@yandex.ru>
2025-09-10 17:07:53 +0300

    Add icons and emodji to location, refactoring

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-    title: 'CRM aquaphor-pro.store',
+    title: 'CRM Aquaphor-pro.store',
     tagline: 'Документация',
     favicon: 'img/aquaphor.png',
 
